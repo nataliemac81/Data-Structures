a422184bfb2df0989a846b6afef5f8adfc22e76f
Natalie McCroy <nataliemccroy@gmail.com>
2020-03-14 12:41:07 -0500

    implemented queue and stack

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,118 @@
-import sys
-sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
+# import sys
+# sys.path.append('../doubly_linked_list')
+# from doubly_linked_list import DoublyLinkedList
 
+
+class ListNode:
+    def __init__(self, value, prev=None, next=None):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+class DoublyLinkedList:
+    def __init__(self, node=None):
+        self.head = node
+        self.tail = node
+        self.length = 1 if node is not None else 0
+
+    def __len__(self):
+        return self.length
+
+    def __str__(self):
+        if self.head is None and self.tail is None: 
+            return "empty"
+        curr_node = self.head
+        output = ''
+        output += f'( {curr_node.value} ) <-> '
+        while curr_node.next is not None:
+            curr_node = curr_node.next 
+            output += f'( {curr_node.value} ) <-> '
+        return output
+
+    def add_to_head(self, value):
+        # adding to an empty list
+        new_node = ListNode(value)   
+        self.length += 1
+        if self.head is None and self.tail is None:
+            # create a new node
+            self.head = new_node
+            self.tail = new_node
+        else:
+            # adding a new value, to existing list
+            # link new_node with current head
+            new_node.next = self.head
+            self.head.prev = new_node
+            # update head
+            self.head = new_node
+
+    def remove_from_head(self):
+        # if list is empty
+        if self.head is None and self.tail is None:
+            return  
+        # if list has only one element
+        elif self.head == self.tail:
+            # unlink the node
+            value = self.head.value
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return value
+        else:
+            # we have more than one element
+            value = self.head.value
+            next_head = self.head.next
+            next_head.prev = None
+            self.head.next = None
+            self.head = next_head
+            self.length -= 1
+            return value
+
+    def add_to_tail(self, value):
+        # adding to an empty list
+        new_node = ListNode(value)   
+        self.length += 1
+        if self.head is None and self.tail is None:
+            # create a new node
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def remove_from_tail(self):
+       # if list is empty
+        if self.head is None and self.tail is None:
+            return  
+        # if list has only one element
+        elif self.head == self.tail:
+            # unlink the node
+            value = self.tail.value
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return value
+        else:
+            # we have more than one element
+            value = self.tail.value
+            prev_tail = self.tail.prev
+            prev_tail.next = None
+            self.tail.prev = None
+            self.tail = prev_tail
+            self.length -= 1
+            return value
 
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        return self.storage.length
